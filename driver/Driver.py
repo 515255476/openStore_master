d7992a1f5d838b64e5b60a4129180c00f4171e35
xinxiaoqiang <515255476@qq.com>
2020-07-15 14:44:46 +0800

    增加hub节点

--- driver/Driver.py
+++ driver/Driver.py
@@ -5,12 +5,16 @@ from selenium import webdriver
 
 
 class Driver(object):
-    options = webdriver.ChromeOptions()
-    options.add_argument('--no-sandbox')
-    options.add_argument('headless')
-    options.add_argument('disable-dev-shm-usage')
-    _driver = webdriver.Chrome(options=options)
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('headless')
+    # options.add_argument('disable-dev-shm-usage')
+    # _driver = webdriver.Chrome(options=options)
     # _driver=webdriver.Chrome()
+    _driver = webdriver.Remote(
+        command_executor='http://10.34.41.10:5001/wd/hub',
+        desired_capabilities={'browserName': 'chrome'}
+    )
     @classmethod
     def start_web(cls):
         cls._driver.implicitly_wait(10)
